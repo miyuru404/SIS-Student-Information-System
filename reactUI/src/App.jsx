92db1fdeee92f4959187f7f96a7f8527a52ce90f
miyuru bashitha <159784923+miyuru404@users.noreply.github.com>
2025-08-13 21:32:55 +0530

    Update App.jsx2

--- reactUI/src/App.jsx
+++ reactUI/src/App.jsx
@@ -11,7 +11,7 @@ async function searchStudent(studentId, setStudent, setError) {
     setError("");
     setStudent(null);
 
-    const response = await fetch(`http://https://sis-backend.onrender.com/students/${studentId.trim()}`);
+    const response = await fetch(`https://sis-backend.onrender.com/students/${studentId.trim()}`);
     if (!response.ok) {
       throw new Error("Student not found");
     }
